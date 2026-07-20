bcf9a87806a3927ff61b2e563a96f89fe6867ae6
Rutuja <rutujadhole1101@gmail.com>
2026-07-21 00:34:32 +0530

    QR Code updated with text

--- 1_QR.py
+++ 1_QR.py
@@ -10,21 +10,25 @@ qr.add_data(url)
 img = qr.make_image()
 
 img = img.convert("RGB")
+#create white background with extra space below
 new_img=Image.new("RGB",(img.width,img.height+120),"white")
 new_img.paste(img,(0,0))
 
+#add text in center(2lines)
 draw = ImageDraw.Draw(new_img)
 font=ImageFont.truetype("arial.ttf",30)
 
 text="Scan QR Code\nTo View Menu"
 draw.multiline_text(
-    (img.width//2,img.height+60),
+    (img.width//2,img.height+60), #center position
     text,
     fill="black",
     font=font,
     align="center",
-    anchor="mm"
+    anchor="mm" #mm means middle-middle 
     )
+
+#save and show msg
 img=new_img
 img.save(file_path)
 
