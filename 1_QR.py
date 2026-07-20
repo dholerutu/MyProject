76f1bb7a6092c565085f8058d17b55ad69411713
Rutuja <rutujadhole1101@gmail.com>
2026-07-20 19:50:21 +0530

    Rename QR file

--- 1_QR.py
+++ 1_QR.py
@@ -1,7 +1,7 @@
 import qrcode
 
 url = input ("Enter the URL: ").strip() #avoid whitespaces
-file_path = r"C:\Users\Rutuja\OneDrive\Git_Project\MyProject\\qrcode.png"
+file_path = r"C:\Users\Rutuja\OneDrive\Git_Project\MyProject\\1_qrcode.png"
 
 qr = qrcode.QRCode() #QR library
 qr.add_data(url)
