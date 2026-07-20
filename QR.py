83c10c273a29a20c412ecc6d892f3a01ba1ece3e
Rutuja <rutujadhole1101@gmail.com>
2026-07-20 19:42:55 +0530

    QR code generator

---
+++ QR.py
@@ -0,0 +1,12 @@
+import qrcode
+
+url = input ("Enter the URL: ").strip() #avoid whitespaces
+file_path = r"C:\Users\Rutuja\OneDrive\Git_Project\MyProject\\qrcode.png"
+
+qr = qrcode.QRCode() #QR library
+qr.add_data(url)
+
+img = qr.make_image()
+img.save(file_path)
+
+print("QR code was generated!")
